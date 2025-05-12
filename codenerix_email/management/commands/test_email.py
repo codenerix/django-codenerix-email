a5ae65d62b6a3a95df1cf51e5943c769b7f69c3c
Juanmi Taboada <juanmi@juanmitaboada.com>
2025-05-12 12:00:28 +0200

    Support for manage.py command 'test_email' which now support extra arguments to improve testing

--- codenerix_email/management/commands/test_email.py
+++ codenerix_email/management/commands/test_email.py
@@ -18,13 +18,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from codenerix_lib.debugger import Debugger
-from codenerix_email.models import EmailMessage
+from codenerix_email.models import EmailMessage, EmailTemplate  # type: ignore
 from codenerix_email import __version__
+from django.core.management import CommandError
 
 
 class Command(BaseCommand, Debugger):
@@ -32,28 +34,127 @@ class Command(BaseCommand, Debugger):
     # Show this when the user types help
     help = "Test"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--email",
+            type=str,
+            help="Email",
+            default=None,
+        )
+        parser.add_argument(
+            "--template",
+            type=str,
+            help="Template CID",
+            default=None,
+        )
+        parser.add_argument(
+            "--context",
+            type=str,
+            help="Context as JSON",
+            default="{}",
+        )
+        parser.add_argument(
+            "--language",
+            type=str,
+            help="Language",
+            default=None,
+        )
+        parser.add_argument(
+            "--stdout",
+            action="store_true",
+            help="Print to stdout",
+            default=False,
+        )
+
     def handle(self, *args, **options):
 
         # Autoconfigure Debugger
         self.set_name("CODENERIX-EMAIL")
         self.set_debug()
 
-        message = """Hello,
+        # Get arguments
+        email = options["email"]
+        template = options["template"]
+        context_str = options["context"]
+        language = options["language"]
+        stdout = options["stdout"]
 
-this email has been sent using Django Codenerix Email.
+        # Read context
+        try:
+            context = json.loads(context_str)
+        except json.JSONDecodeError:
+            raise CommandError(
+                "Context is not a valid JSON string: {}".format(context_str)
+            )
+
+        # If no template is provided, use the default one
+        if template is None:
+
+            # Get the default template
+            message = """Hello,
+
+This email has been sent using Django Codenerix Email.
 
 Best regards, Codenerix Team
 
 --
-Django Codenerix Email v{}""".format(
-            __version__
-        )
+Django Codenerix Email v{}
+""".format(
+                __version__
+            )
 
-        for name, email in settings.ADMINS:
-            email_message = EmailMessage()
+            def email_message_factory(context, language):
+                email_message = EmailMessage()
+                email_message.subject = "[Codenerix Email] Test"
+                email_message.body = message
+                return email_message
+
+        else:
+            # Get the template
+            try:
+                template = EmailTemplate.objects.get(cid=template)
+            except EmailTemplate.DoesNotExist:
+                raise CommandError(
+                    "Template with CID {} does not exist.".format(template)
+                )
+
+            # Render the template
+            def email_message_factory(context, language):
+                return template.get_email(context, language)
+
+        # If no email is provided, send to all admins
+        if email is None:
+
+            # Send email to all admins
+            for name, email in settings.ADMINS:
+                email_message = email_message_factory(context, language)
+                email_message.efrom = settings.DEFAULT_FROM_EMAIL
+                email_message.eto = email
+
+                if stdout:
+                    self.debug(
+                        f"Sending email to {name} <{email}> "
+                        f"with subject: {email_message.subject}:\n"
+                        f"{email_message.body}",
+                        color="white",
+                    )
+                else:
+                    email_message.save()
+                    email_message.send(legacy=False, silent=False)
+        else:
+
+            # Send email to the specified address
+            email_message = email_message_factory(context, language)
             email_message.efrom = settings.DEFAULT_FROM_EMAIL
             email_message.eto = email
-            email_message.subject = "[Codenerix Email] Test"
-            email_message.body = message
-            email_message.save()
-            email_message.send(legacy=False, silent=False)
+
+            if stdout:
+                self.debug(
+                    f"Sending email to {name} <{email}> "
+                    f"with subject: {email_message.subject}:\n"
+                    f"{email_message.body}",
+                    color="white",
+                )
+            else:
+                email_message.save()
+                email_message.send(legacy=False, silent=False)
